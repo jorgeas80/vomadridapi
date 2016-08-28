c00df3ba963e94403c87e8e076a03135ad69f552
Jorge Arevalo <jorgeas80@gmail.com>
2016-08-29 00:20:51 +0200

    Initial commit. Getting all JSON data using mongodb driver. Need to reproduce using mongoose

---
+++ index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var bunyan = require('bunyan');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var mongodb_uri = 'mongodb://localhost:27017/vomadrid';
+
+// Create express app
+var app = express();
+
+// Create log object
+var log = bunyan.createLogger({name: "vomadrid", level: 'debug'});
+
+
+// Allow cors
+app.use(cors());
+
+// Mongodb connect. TODO: Make it configurable!
+mongoose.connect(mongodb_uri);
+
+mongoose.set('debug', function (collectionName, method, query, doc, options) {
+    // do your own custom logging
+    log.debug({'collectionName': collectionName, 'method': method, 'query': query, 'doc': doc});
+})
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function(callback) {
+    log.info('Mongodb connected');
+});
+
+
+var movieSchema = mongoose.Schema({
+    spiders_used: [],
+    movie_id: String,
+    movie_date_added: String,
+    movie_title: String,
+    movie_original_title: String,
+    movie_runtime: Number,
+    movie_gender: String,
+    movie_plot: String,
+    movie_director: String,
+    movie_actors: String,
+    movie_country: String,
+    movie_rating: Number,
+    movie_poster: String,
+    movie_showtimes: []
+});
+
+var gradeSchema = mongoose.Schema({
+    student: String,
+    grade: Number,
+    assignment: String
+});
+
+var movie = mongoose.model('Movie', movieSchema);
+var grade = mongoose.model('Grade', gradeSchema);
+
+app.get('/', function(req, res) {
+    movie.findOne(function(err, doc) {
+        log.info(doc);
+        res.send(doc);
+    });
+
+});
+
+app.get('/yo', function(req, res) {
+    MongoClient.connect(mongodb_uri, function(err, db) {
+        assert.equal(err, null);
+        log.info('Mongodb connected');
+
+        var docsArray = db.collection('vomovies').find().toArray().then(
+            function(data) {
+                res.send(data);
+            },
+
+            function(error) {
+                res.send('Error getting data!');
+            });
+
+    })
+
+    //res.send('YO!');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Server running at http://localhost:' + server.address().port);
+});
